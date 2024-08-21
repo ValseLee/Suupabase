ca787454651880ce1a7f902cb1b831a111e9003c
valselee <sollleky72@gmail.com>
2024-08-21 20:04:48 +0900

    init

---
+++ Suupabase/Suupabase/SuupabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuupabaseApp.swift
+//  Suupabase
+//
+//  Created by Celan on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SuupabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
