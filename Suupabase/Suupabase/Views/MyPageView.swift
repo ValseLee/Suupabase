8dcf4ba0be061ac23e3bcd8908215318794457a0
valselee <sollleky72@gmail.com>
2024-08-25 17:20:31 +0900

    file: 계정 설정 UI 파일 생성

---
+++ Suupabase/Suupabase/Views/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  Suupabase
+//
+//  Created by Celan on 8/25/24.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
